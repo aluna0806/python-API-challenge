f518f306eff9619c403e564085ae9af09d7420e0
aluna0806 <63885788+aluna0806@users.noreply.github.com>
2020-10-10 16:57:19 -0400

    api key and starter code

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "97a01cb065ff58184a44e5419fda38f2"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDyzW6XwfTzusd_2IijvcQ5li3EMH2AV94"
